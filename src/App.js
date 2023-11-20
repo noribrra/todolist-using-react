9eb84c78d1280d10db76ef9cce2ca443c0689510
noribra <noribra@gmail.com>
2023-11-20 18:50:13 +0300

    solve challenge state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [allinput, setalliput] = useState({
+    firstname: "nor",
+    lastname: "",
+    scor: 0,
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div style={{ padding: "100px" }}>
+        <label>scor :{allinput.scor}</label>
+        <button
+          onClick={() => {
+            let num = allinput.scor;
+            setalliput({ ...allinput, scor: num + 1 });
+          }}
         >
-           React
-        </a>
-      </header>
-    </div>
+          +1
+        </button>
+
+        <hr />
+        <label>first name</label>
+        <input
+          value={allinput.firstname}
+          type="text"
+          onChange={(event) => {
+            setalliput({ ...allinput, firstname: event.target.value });
+          }}
+        />
+        <hr />
+        <label>last name</label>
+        <input
+          value={allinput.lastname}
+          type="text"
+          onChange={(event) => {
+            setalliput({ ...allinput, lastname: event.target.value });
+          }}
+        />
+      </div>
+    </>
   );
 }
-
-export default App;
