cf8c504a5833472d41ddadd2d47a81ff1cc6d619
noribra <noribra@gmail.com>
2023-11-20 17:50:40 +0300

    conect all input with state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
+// import Btn from "./But";
+// import Input from "./Input";
+
+import Form from "./Form";
 import "./App.css";
-import Btn from "./But";
-import Input from "./Input";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React
-        </a>
-        <Btn />
-        <Input />
-      </header>
+    <div
+      className="App"
+      style={{
+        paddingTop: "300px",
+      }}
+    >
+      <Form />
     </div>
   );
 }
