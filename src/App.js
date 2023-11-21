cd25162f9eee7dcc9ec3387bfe5b1b3b321c1063
noribra <noribra@gmail.com>
2023-11-21 11:16:32 +0300

    challenge 1

--- src/App.js
+++ src/App.js
@@ -1,72 +1,52 @@
-import "./App.css";
 import { useState } from "react";
-let next = 5;
 
-function App() {
-  const [name, setname] = useState("");
-  const [mname, setmname] = useState([
-    { id: 1, nn: "mac" },
-    { id: 2, nn: "iphone" },
-    { id: 3, nn: "samsung" },
-    { id: 4, nn: "windows" },
-  ]);
-  const divices = mname.map((div) => {
-    return (
-      <li key={div.id} style={{ margin: "20px" }}>
-        {div.nn}{" "}
-        <button
-          onClick={() => {
-            deleteclick(div.id);
-          }}
-        >
-          Delete
-        </button>
-        <button
-          onClick={() => {
-            editclick(div.id);
-          }}
-        >
-          edit nor
-        </button>
-      </li>
-    );
-    function deleteclick(id) {
-      const newd = mname.filter((divv) => {
-        return divv.id !== id;
-      });
-      setmname(newd);
-    }
-    function editclick(id) {
-      const newdiv = mname.map((divs) => {
-        if (divs.id === id) {
-          let newobj = { ...divs, nn: divs.nn + "nor" };
-          return newobj;
-        } else {
-          return divs;
-        }
-      });
-      setmname(newdiv);
-    }
-  });
-  return (
-    <div className="App">
-      {divices}
-      <div>
-        <input
-          value={name}
-          type="text"
-          onChange={(e) => {
-            setname(e.target.value);
-          }}
-        />
-        <button onClick={click}>add</button>
-      </div>
-    </div>
-  );
-  function click() {
-    setmname([...mname, { id: next, nn: name }]);
-    next = next + 1;
+const initialProducts = [
+  {
+    id: 0,
+    name: "Baklava",
+    count: 1,
+  },
+  {
+    id: 1,
+    name: "Cheese",
+    count: 5,
+  },
+  {
+    id: 2,
+    name: "Spaghetti",
+    count: 2,
+  },
+];
+
+export default function App() {
+  const [products, setProducts] = useState(initialProducts);
+
+  function handleIncreaseClick(productId) {
+    let nwearray = products.map((obj) => {
+      if (obj.id === productId) {
+        let newobj = { ...obj, count: obj.count + 1 };
+        return newobj;
+      } else {
+        return obj;
+      }
+    });
+    setProducts(nwearray);
   }
-}
 
-export default App;
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
