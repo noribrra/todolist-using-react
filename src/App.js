c43c4d4eac8c91d5fd10d57202356f50d2a06ac3
noribra <noribra@gmail.com>
2023-11-21 12:29:58 +0300

    ll

--- src/App.js
+++ src/App.js
@@ -1,72 +1,20 @@
-import "./App.css";
 import { useState } from "react";
-let next = 5;
+import "./App.css";
 
 function App() {
-  const [name, setname] = useState("");
-  const [mname, setmname] = useState([
-    { id: 1, nn: "mac" },
-    { id: 2, nn: "iphone" },
-    { id: 3, nn: "samsung" },
-    { id: 4, nn: "windows" },
-  ]);
-  const divices = mname.map((div) => {
-    return (
-      <li key={div.id} style={{ margin: "20px" }}>
-        {div.nn}{" "}
-        <button
-          onClick={() => {
-            deleteclick(div.id);
-          }}
-        >
-          Delete
-        </button>
-        <button
-          onClick={() => {
-            editclick(div.id);
-          }}
-        >
-          edit
-        </button>
-      </li>
-    );
-    function deleteclick(id) {
-      const newd = mname.filter((divv) => {
-        return divv.id !== id;
-      });
-      setmname(newd);
-    }
-    function editclick(id) {
-      const newdiv = mname.map((divs) => {
-        if (divs.id === id) {
-          let newobj = { ...divs, nn: divs.nn + "nor" };
-          return newobj;
-        } else {
-          return divs;
-        }
-      });
-      setmname(newdiv);
-    }
-  });
+  const [count, setcount] = useState(0);
+  function add() {
+    setcount(count + 1);
+    setcount((c) => {
+      return c + 1;
+    });
+  }
   return (
     <div className="App">
-      {divices}
-      <div>
-        <input
-          value={name}
-          type="text"
-          onChange={(e) => {
-            setname(e.target.value);
-          }}
-        />
-        <button onClick={click}>add</button>
-      </div>
+      <h1> the Count is:{count}</h1>
+      <button onClick={add}>+</button>
     </div>
   );
-  function click() {
-    setmname([...mname, { id: next, nn: name }]);
-    next = next + 1;
-  }
 }
 
 export default App;
